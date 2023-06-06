1be129a32ffa5b0a9066b7881fbcb59eec3d18b9
Joyclare <cheronojoyclaire@gmail.com>
2023-06-06 01:36:53 -0700

    Python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-# Author - Godswill Kalu
+# Author - Joyclare Cherono
+
 for letter in range(97, 123):
     if chr(letter) != 'q' and chr(letter) != 'e':
         print("{}".format(chr(letter)), end="")

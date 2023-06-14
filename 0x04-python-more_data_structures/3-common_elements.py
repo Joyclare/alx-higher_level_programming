542f59f6dfe2204e89b1f4b7e3c6a307dea29347
Joyclare <cheronojoyclaire@gmail.com>
2023-06-14 03:59:01 -0700

    Python

---
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+# Author - Joyclare Cherono
+
+
+def common_elements(set_1, set_2):
+    """
+    A function that returns a set of
+    common elements in two sets
+    """
+    return(set_1 & set_2)

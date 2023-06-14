909412ea59d94ae4bd0c58940a50334f5efce09b
Joyclare <cheronojoyclaire@gmail.com>
2023-06-14 04:05:37 -0700

    Python

---
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+# Author - Joyclare Cherono
+
+def number_keys(a_dictionary):
+    """
+    A function that returns the number of keys in a dictionary.
+    """
+    return len(a_dictionary)

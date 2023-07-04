567b22c29a32390fe1cdce1186bb4369dd4fd99a
Joyclare <cheronojoyclaire@gmail.com>
2023-07-04 08:36:26 -0700

    Updated

---
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+# Author - Joyclare Cherono
+
+def magic_string():
+    magic_string.n = getattr(magic_string, 'n', 0) + 1
+    return ("BestSchool, " * (magic_string.n - 1) + "BestSchool")
---
+++ 0x09-python-everything_is_object/19-copy_list.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python3
+# Author - Joyclare Cherono
+def copy_list(a):
+    return a.copy()

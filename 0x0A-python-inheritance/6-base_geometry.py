1f367483ebc733ec42aeebf17f7caec5371d2741
Joyclare <cheronojoyclaire@gmail.com>
2023-07-10 09:14:29 -0700

    Updated

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+# Author - Joyclare Cherono
+
+"""Defines a base geometry class BaseGeometry."""
+
+
+class BaseGeometry:
+    """Represent base geometry."""
+
+    def area(self):
+        """Not implemented."""
+        raise Exception("area() is not implemented")

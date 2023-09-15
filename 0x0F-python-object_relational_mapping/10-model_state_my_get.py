c7dc48d10b18bfb36b2fa1b809003129531c3d47
Joyclare <cheronojoyclare@gmail.com>
2023-09-15 12:23:51 -0700

    SQL

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,42 +1,27 @@
 #!/usr/bin/python3
-"""
-Prints the State object with the name passed as argument from
-the database hbtn_0e_6_usa
-"""
+"""A script that prints the State passed as argument
+from the database hbtn_0e_6_usa"""
 
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
+from sys import argv
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2],
-                                  sys.argv[3], sys.argv[4]),
-                           pool_pre_ping=True)
 
-    # Initialize engine
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    # Initialize session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-    query = session.query(State).all()
-
-    # Create all name for states
-    list_state = []
-    for state in query:
-        list_state.append(state.name)
-
-    # Query
-    query = session.query(State).\
-        filter(State.name == "{}".format(sys.argv[4], )).first()
-
-    # Conditions
-    if sys.argv[4] not in list_state:
-        print("Not found")
+    session = Session(engine)
+    state = session.query(State).filter(State.name == sys.argv[4]).first()
+    if state:
+        print("{}".format(state.id))
     else:
-        print(query.id)
-
-    # Close session
+        print("Not found")
     session.close()

15a6d52fd3717ff42d10e17a08e29a9ee0455953
Joyclare <cheronojoyclare@gmail.com>
2023-09-15 12:26:04 -0700

    SQL

--- 0x0F-python-object_relational_mapping/11-model_state_insert.py
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -1,37 +1,27 @@
 #!/usr/bin/python3
-"""
-Adds the State object “Louisiana” to
-the database hbtn_0e_6_usa
-"""
+"""A script that adds object “Louisiana”
+to the database hbtn_0e_6_us"""
 
-import sys
-from model_state import Base, State
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+
+from sys import argv
+
 
 if __name__ == "__main__":
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(sys.argv[1], sys.argv[2], sys.argv[3]),
-                           pool_pre_ping=True)
 
-    # Initialize engine
+    import sys
+    from model_state import Base, State
+    from sqlalchemy import create_engine
+    from sqlalchemy.orm import Session
+
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2],
+                                   sys.argv[3]), pool_pre_ping=True)
     Base.metadata.create_all(engine)
 
-    # Initialize session
-    Session = sessionmaker(bind=engine)
-    session = Session()
-
-    # Add new record
-    new_state = State(name="Louisiana")
-    session.add(new_state)
+    session = Session(engine)
+    new_st = State(name='Louisiana')
+    session.add(new_st)
+    new_state = session.query(State).filter(State.name == 'Louisiana').first()
     session.commit()
-
-    # Query
-    query = session.query(State).\
-        order_by(State.id.desc()).first()
-
-    # Print query
-    print("{}".format(query.id))
-
-    # Close session
+    print('{}'.format(new_state.id))
     session.close()

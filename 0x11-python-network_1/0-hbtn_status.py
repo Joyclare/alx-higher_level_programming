433c133551b85197941a6d7fa715e22abe3fe7c5
Joyclare <cheronojoyclare@gmail.com>
2023-09-29 12:36:44 -0700

    Updated

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python3
-"""What's my status?"""
-import urllib.request
+"""Python script Fetches https://intranet.hbtn.io/status."""
+if __name__ == "__main__":
+    import urllib.request
 
-with urllib.request.urlopen('https://intranet.hbtn.io/status') as url:
-    s = url.read()
-    req = "Body response:\n\t- type: {}\n\t- content: {}\n\t- utf8 content: {}"
-    print(req.format(type(s), s, s.decode('utf-8')))
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status')\
+            as response:
+        html = response.read()
+
+        print('Body response:')
+        print('\t- type: {}'.format(type(html)))
+        print('\t- content: {}'.format(html))
+        print('\t- utf8 content: {}'.format(html.decode("utf-8", "replace")))
